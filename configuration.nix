9294002980337ff5df04c9c5f9c3d3d0d4574f93
Serra Kangas <kangasgames789@gmail.com>
2025-08-23 18:15:34 -0500

    disabled ssh for now

--- configuration.nix
+++ configuration.nix
@@ -162,18 +162,18 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    ports = [ 22 ];
-    settings = {
-      PasswordAuthentication = true;
-      AllowUsers = [ "kangn8r" ]; # Allows all users by default. Can be [ "user1" "user2" ]
-      UseDns = true;
-      X11Forwarding = false;
-      PermitRootLogin = "no"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
-      openFirewall = true;
-    };
-  };
+  # services.openssh = {
+  #   enable = true;
+  #   ports = [ 22 ];
+  #   settings = {
+  #     PasswordAuthentication = true;
+  #     AllowUsers = [ "kangn8r" ]; # Allows all users by default. Can be [ "user1" "user2" ]
+  #     UseDns = true;
+  #     X11Forwarding = false;
+  #     PermitRootLogin = "no"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+  #   };
+  # };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
