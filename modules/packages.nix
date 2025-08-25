ba8b0be337013b6d7f761c0152ce7791e734dfb8
Serra Kangas <kangasgames789@gmail.com>
2025-08-24 22:55:49 -0500

    dust and mangal

--- modules/packages.nix
+++ modules/packages.nix
@@ -15,6 +15,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    dust
+    mangal
     josm
     zellij
     wget
