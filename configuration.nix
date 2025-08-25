cab945510c6864a92b1daeb22af11f786d5d6de9
Serra Kangas <kangasgames789@gmail.com>
2025-08-24 20:31:41 -0500

    wireshark stuff, cleaned up excess package declarations

--- configuration.nix
+++ configuration.nix
@@ -89,6 +89,7 @@
     extraGroups = [
       "networkmanager"
       "wheel"
+      "wireshark"
     ];
     packages = with pkgs; [
       kdePackages.kate
@@ -104,46 +105,6 @@
     "flakes"
   ];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    wget
-    neovim
-    tmux
-    fastfetch
-    tmuxp
-    ranger
-    fzf
-    git
-    mc
-    python314
-    unzip
-    nodejs_24
-    vesktop
-    btop
-    gh
-    heroic
-    btop
-    umu-launcher
-    home-manager
-    pipes-rs
-    crawlTiles
-    gamemode
-    mangohud
-    gitui
-    gcc
-    cargo
-    lua-language-server
-    stylua
-    nixfmt-rfc-style
-    libreoffice-qt6-fresh-unwrapped
-    cbonsai
-    cowsay
-  ];
-
-  fonts.packages = with pkgs; [
-    nerd-fonts.fira-code
-  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -174,6 +135,11 @@
     maxretry = 5;
     bantime = "24h";
 
+  };
+
+  programs.wireshark = {
+    dumpcap.enable = true;
+    usbmon.enable = true;
   };
 
   # Open ports in the firewall.
