60fbefcf1517706c84548b04942ac87e4512269e
Serra Kangas <kangasgames789@gmail.com>
2025-08-30 19:32:02 -0500

    add eza

--- modules/packages.nix
+++ modules/packages.nix
@@ -15,6 +15,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    eza
     thunderbird
     dysk
     puddletag
