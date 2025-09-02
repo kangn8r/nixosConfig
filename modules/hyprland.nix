fb60d06e5c5aaa278c1100b3ae0d317e4d339521
Serra Kangas <kangasgames789@gmail.com>
2025-09-02 01:33:32 -0500

    hyperland stuff

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,15 +1,20 @@
 { pkgs, ... }:
 
 {
+
   # Install firefox.
   programs.hyprland.enable = true;
   # List packages installed in system profile. To search, run:
   environment.systemPackages = with pkgs; [
+    brightnessctl
+    dunst
+    grimblast
+    networkmanagerapplet
     hyprpaper
     hyprlock
     hyprls
-    wofi
+    rofi
     waybar
   ];
-  
+
 }
