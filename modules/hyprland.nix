069938efec4ce2f4f36a335522f4fbbc65a779e3
Serra Kangas <kangasgames789@gmail.com>
2025-09-02 01:37:47 -0500

    add hyperpolkit

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -6,6 +6,7 @@
   programs.hyprland.enable = true;
   # List packages installed in system profile. To search, run:
   environment.systemPackages = with pkgs; [
+    hyprpolkitagent
     brightnessctl
     dunst
     grimblast
