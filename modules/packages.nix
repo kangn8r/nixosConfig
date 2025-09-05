93156efc91193e100ac7aed2358b4a386c4830b8
Serra Kangas <kangasgames789@gmail.com>
2025-09-05 13:15:43 -0500

    the greatest games ever =)

--- modules/packages.nix
+++ modules/packages.nix
@@ -14,6 +14,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    wesnoth
+    superTux
     ripgrep
     fortune
     eza
