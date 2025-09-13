c88673eae56e369b10ace202f0a56066b43f0da3
Serra Kangas <kangasgames789@gmail.com>
2025-09-13 02:20:15 -0500

    everest mons added

--- modules/packages.nix
+++ modules/packages.nix
@@ -37,6 +37,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    everest-mons
     zsh-powerlevel10k
     wesnoth
     superTux
