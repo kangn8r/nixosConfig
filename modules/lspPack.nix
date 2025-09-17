3e4bdd2361e1272dcb8fb7c23b3f14a069c3e541
Serra Kangas <kangasgames789@gmail.com>
2025-09-17 08:05:47 -0500

    fix rebase

--- modules/lspPack.nix
+++ modules/lspPack.nix
@@ -4,13 +4,9 @@
   environment.systemPackages = with pkgs; [
     jdt-language-server
     stylua
-    lua-language-server
     nil
     python313Packages.debugpy
     python313Packages.python-lsp-server
-    hyprls
-    lua-language-server
-		python313Packages.python-lsp-server
 		python313Packages.rope
 		python313Packages.pyflakes
 		hyprls
